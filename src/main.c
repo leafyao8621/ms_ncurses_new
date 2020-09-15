e6df5c0416175bf6069db1aafd6344f0103b1ae1
Yi Yao <leaf_yao8621@hotmail.com>
2020-09-15 17:32:10 -0400

    Added additional layout

--- src/main.c
+++ src/main.c
@@ -13,23 +13,35 @@ int main(void) {
         ch = getch();
         switch (ch) {
         case KEY_UP:
+        case 'W':
+        case 'w':
             controller_move(CONTROLLER_UP);
             break;
         case KEY_DOWN:
+        case 'S':
+        case 's':
             controller_move(CONTROLLER_DOWN);
             break;
         case KEY_LEFT:
+        case 'A':
+        case 'a':
             controller_move(CONTROLLER_LEFT);
             break;
         case KEY_RIGHT:
+        case 'D':
+        case 'd':
             controller_move(CONTORLLER_RIGHT);
             break;
         case 'Z':
         case 'z':
+        case 'J':
+        case 'j':
             out = controller_check();
             break;
         case 'X':
         case 'x':
+        case 'K':
+        case 'k':
             controller_mark();
             break;
         case 'Q':
